6beb4cfbac54e6ba604313501d9fb82da20d2e10
carloscsc <carlos.azarro@gmail.com>
2024-12-19 00:50:08 -0300

    refactor: update insertion sort tests for clarity and accuracy

--- src/sort/sort.spec.ts
+++ src/sort/sort.spec.ts
@@ -1,8 +1,12 @@
 import insertionSort from './Insertion-sort'
-describe('Sort Tests', () => {
+describe('Insertion Sort', () => {
 	it('should sort an array in ascending order', () => {
-		const arr = [5, 3, 8, 4, 2, 1, 9, 7, 1, 6]
+		const arr = [5, 3, 8, 4, 2, 1, 9, 7, 6]
+		expect(insertionSort(arr)).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9])
+	})
 
-		expect(insertionSort(arr)).toEqual([1, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+	it('should sort an array with repetead values in ascrendig order', () => {
+		const arr = [0, 1, 4, 6, 5, 1, 0, 1, 3]
+		expect(insertionSort(arr)).toEqual([0, 0, 1, 1, 1, 3, 4, 5, 6])
 	})
 })
